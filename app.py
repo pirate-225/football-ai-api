8b5c66349ad9d38891445ff7df6e1a3dbd9cf23b
smokey <abdoul_doki@hotmail.com>
2026-04-09 20:13:52 +0200

    fix render port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import os
 from predict_match import predict_match
 from api.get_today_matches import get_today_matches
 
@@ -10,14 +11,10 @@ teams_df = pd.read_csv("data_processed/team_stats.csv")
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-    print("🔥 ROUTE INDEX APPELÉE")
+    print("🔥 APP RUNNING")
 
     result = None
-
-    print("📡 Appel API matchs du jour...")
     today_matches = get_today_matches()
-
-    print("📊 Matchs récupérés :", len(today_matches))
 
     if request.method == "POST":
         home_team = request.form["home_team"]
@@ -36,5 +33,7 @@ def index():
         today_matches=today_matches
     )
 
+# 🔥 IMPORTANT POUR RENDER
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
