320b786ee1fc466f4d020ecd8e48c74e090b60bc
smokey <abdoul_doki@hotmail.com>
2026-04-15 00:21:18 +0200

    test predict only

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pandas as pd
+import os
+
+from predict_match import predict_match
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "OK WORKING"
+try:
+    teams = pd.read_csv("data_processed/team_stats.csv")
+except:
+    teams = pd.DataFrame()
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    result = None
+
+    if request.method == "POST":
+
+        home = request.form.get("home_team")
+        away = request.form.get("away_team")
+
+        if home and away:
+
+            result = predict_match(home, away, 2, 3, 4)
+
+    return render_template(
+        "index.html",
+        teams=teams,
+        result=result,
+        top_bets=[]
+    )
+
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
